e3d6950bb232b3147efc65a780bfe5ba1b6a9bde
jdplife <527688483@qq.com>
2023-02-21 18:53:22 +0800

    Add files via upload

---
+++ rabbit-consumer/src/main/java/com/jdp/rabbitmq/topicconfig/TopicRabbitConfig.java
@@ -0,0 +1,53 @@
+package com.jdp.rabbitmq.topicconfig;
+
+/**
+ * @ClassName TopicRabbitConfig
+ * @Description TODO
+ * @Author jdp
+ * @Date 13:39 2023/2/17
+ * @Version 1.0
+ **/
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TopicRabbitConfig {
+    //绑定键
+    public final static String man = "topic.man1";
+    public final static String woman = "topic.woman1";
+
+    @Bean
+    public Queue firstQueue() {
+        return new Queue(TopicRabbitConfig.man);
+    }
+
+    @Bean
+    public Queue secondQueue() {
+        return new Queue(TopicRabbitConfig.woman);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("topicExchange");
+    }
+
+    //将firstQueue和topicExchange绑定,而且绑定的键值为topic.man
+    //这样只要是消息携带的路由键是topic.man,才会分发到该队列
+    @Bean
+    Binding bindingExchangeMessage() {
+        return BindingBuilder.bind(firstQueue()).to(exchange()).with(man);
+    }
+
+    //将secondQueue和topicExchange绑定,而且绑定的键值为用上通配路由键规则topic.#
+    // 这样只要是消息携带的路由键是以topic.开头,都会分发到该队列
+    @Bean
+    Binding bindingExchangeMessage2() {
+        return BindingBuilder.bind(secondQueue()).to(exchange()).with(woman);
+    }
+
+}
+
